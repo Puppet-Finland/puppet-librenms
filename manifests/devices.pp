dc737c9de15fa919f7226d8b58b1bf7d0e4812a1
Samuli Seppänen <samuli@openvpn.net>
2016-06-14 06:54:24 +0000

    Move device adding to a more appropriately named class
    Signed-off-by: Samuli Seppänen <samuli@openvpn.net>

--- manifests/devices.pp
+++ manifests/devices.pp
@@ -1,10 +1,10 @@
 #
-# == Class: librenms::server
+# == Class: librenms::devices
 #
 # This class is included on the librenms server. Currently it's only purpose is 
 # to automatically add all Puppet-managed nodes to the librenms database.
 #
-class librenms::server {
+class librenms::devices {
 
     # Add all Puppet-managed nodes ("devices") to librenms
     Exec <<| tag == 'librenms-add_device' |>> {
