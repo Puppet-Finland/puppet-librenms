13088bcd69c5c47d0cbdc36e95c7fd4e87628234
Samuli Seppänen <samuli@openvpn.net>
2020-03-04 09:49:08 +0200

    Vagrant: ensure that LibreNMS is set up before adding hosts
    Signed-off-by: Samuli Seppänen <samuli@openvpn.net>

--- vagrant/librenms.pp
+++ vagrant/librenms.pp
@@ -90,4 +90,6 @@ class { '::librenms::device':
   user    => $snmp_user,
   pass    => $snmp_pass,
   realize => true,
+  # Ensure that LibreNMS is fully setup before we try to add this node this it
+  require => [ Snmpd::User[$snmp_user], Class['::librenms'], ],
 }
