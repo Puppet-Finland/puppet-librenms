b2b60f555326dd9d0af99e7b08cce34a9e504d9f
Samuli Seppänen <samuli.seppanen@puppeteers.fi>
2021-01-30 10:54:06 +0200

    Vagrant: switch to using Puppet 6 agent
    Signed-off-by: Samuli Seppänen <samuli.seppanen@puppeteers.fi>

--- vagrant/debian.sh
+++ vagrant/debian.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo I am provisioning...
 export FACTER_is_vagrant='true'
-wget https://raw.githubusercontent.com/Puppet-Finland/scripts/3c1cf163edeebceebd4a29c7c28e6e3a4a11c319/bootstrap/linux/install-puppet.sh -q -O install-puppet.sh
+wget https://raw.githubusercontent.com/Puppet-Finland/scripts/66badaff297b4ba1ff0d71d589198e17b0f28a06/bootstrap/linux/install-puppet.sh -q -O install-puppet.sh
 /bin/sh install-puppet.sh
